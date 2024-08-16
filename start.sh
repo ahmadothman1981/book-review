3a52c60b4b315749b0d45dbf9bce14638ea0ed47
ahmadothman1981 <ahmadothman@outlook.com>
2024-08-16 20:39:45 +0300

    start again

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-if ! command -v php &> /dev/null
+
+# Locate PHP executable
+PHP_PATH=$(command -v php)
+
+if [ -z "$PHP_PATH" ]
 then
     echo "PHP could not be found. Please install PHP and try again."
     exit 1
 fi
 
-php artisan serve --host=0.0.0.0 --port=80
+# Start Laravel application
+$PHP_PATH artisan serve --host=0.0.0.0 --port=80
